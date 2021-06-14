a8baebcedf167594e830e594366015397c31d214
Colton <watkincw@gmail.com>
2021-06-13 22:16:10 -0500

    add ability to submit custom tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,14 @@ var taskFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    // check if input calues are empty strings
+    if (!taskNameInput || !taskTypeInput) { 
+        alert("You need to fill out the fask form!");
+        return false;
+    }
+
+    formEl.reset();
+
     // package up data as an object
     var taskDataObj = { 
         name: taskNameInput,
